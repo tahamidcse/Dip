3954ded090f1a85a33d3aa7f1a1a71ec03a5ae7a
MD. TAHAMID HASAN <112802620+tahamidcse@users.noreply.github.com>
2025-09-15 00:56:25 -0400

    Create morph.py

---
+++ morph.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+#built in + own salt and pepper noise remove
+def main():
+    img=cv2.imread('FigP0433(left)(DIP_image).tif',0)
+    ker=np.ones((3,3),dtype=np.uint8)
+    ker2=np.ones((5,5),dtype=np.uint8)
+    eimg=cv2.erode(img,ker,iterations=2)
+    dimg=cv2.dilate(mimg,ker,iterations=2)
+    cv2.imshow('Original Image', img)
+    cv2.imshow('Eroded Image', mimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+if __name__=="__main__":
+   main()
